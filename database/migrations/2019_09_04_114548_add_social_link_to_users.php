f2d3ee1765de6413d222fb2281a95a04ed78dc55
Hosam Mohamed <hosammohamed9@gmail.com>
2019-09-04 23:07:42 +0200

    updates

--- database/migrations/2019_09_04_114548_add_social_link_to_users.php
+++ database/migrations/2019_09_04_114548_add_social_link_to_users.php
@@ -14,7 +14,7 @@ class AddSocialLinkToUsers extends Migration
     public function up()
     {
         Schema::table('users', function($table) {
-            $table->string('socialLink');
+            $table->string('socialLink')->nullable();
         });
     }
 
