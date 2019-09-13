f1d751959255ebee63fc0d5bd5641ab76f6d0d35
Hosam Mohamed <hosammohamed9@gmail.com>
2019-09-13 04:05:23 +0200

    updates

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Http\Request;
-
+header('Access-Control-Allow-Origin', '*');
+header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 Route::group([
     'prefix' => 'auth'
 ], function () {
