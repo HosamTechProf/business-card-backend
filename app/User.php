631fe63ef620404f624209c1834c0adf4bb51903
Hosam Mohamed <hosammohamed9@gmail.com>
2019-09-15 00:31:57 +0200

    updates

--- app/User.php
+++ app/User.php
@@ -23,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','phone','desc','company','mobile','isPublic','socialLink'
+        'name', 'email', 'password','phone','desc','company','mobile','isPublic','socialLink','deviceToken'
     ];
 
     /**
