3c1af5f6b8a35a0e079cc413499ade106766fb02
Hosam Mohamed <hosammohamed9@gmail.com>
2019-11-10 01:57:08 +0200

    updates

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('phone')->nullable();
-            $table->string('mobile');
+            $table->string('mobile')->unique();
             $table->string('company');
             $table->string('desc');
             $table->string('email')->unique();
